5a30ee27704b6620ec9402af7cf867a19137de81
nsurya-0698 <nsuryateja1997@gmail.com>
2024-07-20 17:07:49 -0500

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from '@svgr/rollup'; // import svgr plugin
-
-export default defineConfig({
-  base: '/createURLForReactApp/',
-  plugins: [react(), svgr()],
-});
